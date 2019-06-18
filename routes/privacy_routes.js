cf410071beef51613a926bf33751533a0b0cf160
Bibek1 <bibek.gurung1@nhs.net>
2019-06-18 15:44:01 +0100

    Privacy policy accept now works for staff user types

--- routes/privacy_routes.js
+++ routes/privacy_routes.js
@@ -6,6 +6,7 @@ const databaseLogger = require('../config/winston-db')
 
 let PrivacyRecords = require('../models/gdpr.model');
 let Trainee = require('../models/trainee.model');
+let User = require('../models/staff');
 var moment = require('moment');
 
 
@@ -39,7 +40,26 @@ privacyRoutes.get('/accept/:id', function(req, res){
     console.log(req.params.id);
         Trainee.findById(req.params.id, function(err, trainee){
             if(!trainee){
-                winston.error("could not find trainee!" + +moment().format('h:mm:ss a'));
+                User.findById(req.params.id, function(err, user){
+                    if(!user){
+                        winston.error("could not find user or trainee!" + +moment().format('h:mm:ss a'));
+                    }
+                    else{
+                        let email = CryptoJS.AES.decrypt(user.email
+                            , CryptoJS.enc.Hex.parse("253D3FB468A0E24677C28A624BE0F939")
+                            , { iv: CryptoJS.enc.Hex.parse("00000000000000000000000000000000") })
+                            .toString(CryptoJS.enc.Utf8);
+                        let logger = databaseLogger.createLogger(email);
+                        let logUser = req.params.id;
+                        let permission = CryptoJS.AES.encrypt('true', '3FJSei8zPx').toString();
+                        let newRecord = new PrivacyRecords({ permission: permission, user: logUser });
+                        console.log(newRecord);
+                        newRecord.save();
+                        logger.info('Privacy notice has been accepted ' + moment().format('h:mm:ss a'));
+                        winston.info('Privacy notice has been accepted ' + moment().format('h:mm:ss a'));
+                        res.send('Success');
+                    }
+                })
             }
             else{
                 let email = CryptoJS.AES.decrypt(trainee.trainee_email
