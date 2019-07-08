21d4a8518953d94d91742e099953a69f346756df
Bibek1 <bibek.gurung1@nhs.net>
2019-07-08 17:31:12 +0100

    populating approvedby fields

--- models/monthlyReport.model.js
+++ models/monthlyReport.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+var CryptoJS = require("crypto-js");
 
 
 const MonthlySchema = new mongoose.Schema({
@@ -25,3 +26,23 @@ const MonthlySchema = new mongoose.Schema({
 );
 
 module.exports = mongoose.model('Monthly.Report', MonthlySchema);
+
+let rep = module.exports = mongoose.model('Monthly.Report', MonthlySchema);
+
+rep.find({}, function(err, reports){
+  console.log("reaches here");
+  if(!err){
+      reports.map(report => {
+          if(report.approvedBy === undefined){
+              report.approvedBy = CryptoJS.AES.encrypt("", '3FJSei8zPx').toString();
+          }
+          if(report.financeApprove === undefined){
+              report.financeApprove = CryptoJS.AES.encrypt("", '3FJSei8zPx').toString();
+          }
+          report.save();
+      })
+  }
+  else{
+      throw err;
+  }
+})
